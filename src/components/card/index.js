87d65d69f2ee17e23badcbc51e22f75aacfbe825
Kiran D Cunha <kiran.dcunha@ltts.com>
2020-10-31 20:36:18 +0530

    added ellpsis component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,26 +1,34 @@
 import React from "react";
+import LinesEllipsis from "react-lines-ellipsis";
 import {SERVER_URL} from "../../utils/constants";
-import defaultImage from'../../assets/placeholder_for_missing_posters.png';
+import defaultImage from "../../assets/placeholder_for_missing_posters.png";
 import "./index.css";
 
 class Card extends React.Component {
   constructor(props) {
-    super(props)
-    this.onError = this.onError.bind(this)
+    super(props);
+    this.onError = this.onError.bind(this);
   }
   onError(event) {
-    event.target.src = defaultImage
+    event.target.src = defaultImage;
   }
   render() {
     return (
       <div className="card">
         <img
-          onError= {this.onError}
+          onError={this.onError}
           src={SERVER_URL + this.props.poster["poster-image"]}
           className="poster"
           alt="poster"
         />
-        <div className="name">{this.props.poster["name"]}</div>
+        <LinesEllipsis
+        className="name"
+          text={this.props.poster["name"]}
+          maxLine="2"
+          ellipsis="..."
+          trimRight
+          basedOn="letters"
+        />
       </div>
     );
   }
